23da15162cca14e6b7ff967df639232c620f8e73
hasnattanvir <hasnattanvir014@gmail.com>
2021-08-03 19:07:36 +0600

    final

--- assets/js/virusupdate.js
+++ assets/js/virusupdate.js
@@ -6,13 +6,15 @@ fetch('https://corona.lmao.ninja/v2/countries/bangladesh')
         console.log(data);
         document.getElementById("flag").src = data.countryInfo.flag;
         // document.getElementById("country").innerHTML = data.country;
-
         document.getElementById("cases").innerHTML = data.cases.toLocaleString();
 
-        document.getElementById("critical").innerHTML = data.critical.toLocaleString();
+        // document.getElementById("critical").innerHTML = data.critical.toLocaleString();
         document.getElementById("deaths").innerHTML = data.deaths.toLocaleString();
         document.getElementById("recovery").innerHTML = data.recovered.toLocaleString();
         document.getElementById("tests").innerHTML = data.tests.toLocaleString();
         document.getElementById("active").innerHTML = data.active.toLocaleString();
+        document.getElementById("todayDeaths").innerHTML = data.todayDeaths.toLocaleString();
+
+
 
     })
